2012731910e59f01db1c791bc2f4f435e92160f9
yonnybarrios <yonnyabarriosp@gmail.com>
2024-04-23 21:56:10 -0400

    Revert "Cambios footer"
    This reverts commit 58f86583ff05ad5038cb6a14b4685d714fc45531.

--- Admi-Panel/Admi-Desktop/src/pages/AjustesDelSistemas/InformacionGeneral.jsx
+++ Admi-Panel/Admi-Desktop/src/pages/AjustesDelSistemas/InformacionGeneral.jsx
@@ -1,162 +1,143 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { TextField, Button, Box, Grid, Alert } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+} from "@mui/material";
+import { styled } from "@mui/system";
 import SidebarMui from "../../components/sideBar/SidebarMui";
-import "./informacionGeneral.css";
+import "./informacionGeneral.css"
+const StyledButton = styled(Button)({
+  margin: "10px",
+});
 
 export default function InformacionGeneral() {
- const [formData, setFormData] = useState({
-    nombreEmpresa: "",
-    rifEmpresa: "",
-    telefono: "",
-    ubicacion: "",
-    correo: "",
-    linkFacebook: "",
-    linkInstagram: "",
- });
- const [submittedData, setSubmittedData] = useState(null);
- const [message, setMessage] = useState("");
- const [messageType, setMessageType] = useState("");
+  const [logo, setLogo] = useState(null);
+  const [formData, setFormData] = useState({
+    companyName: "",
+    rif: "",
+    mission: "",
+    vision: "",
+    corporateColor: "",
+  });
 
- const handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
- };
+  };
 
- const handleSubmit = async (event) => {
+  const handleFileChange = (event) => {
+    setLogo(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    console.log(formData, logo);
+  };
 
-    const formDataToSend = new FormData();
-    Object.keys(formData).forEach((key) => {
-      formDataToSend.append(key, formData[key]);
-    });
-
-    try {
-      const response = await axios.post("http://localhost:3000/informacionGeneral/post-infoGeneral", formDataToSend, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-
-      if (response.status === 200) {
-        setMessage("Datos guardados con éxito");
-        setMessageType("success");
-        setSubmittedData(formData);
-        setFormData({
-          nombreEmpresa: "",
-          rifEmpresa: "",
-          telefono: "",
-          ubicacion: "",
-          correo: "",
-          linkFacebook: "",
-          linkInstagram: "",
-        });
-      }
-    } catch (error) {
-      setMessage("Error al guardar los datos");
-      setMessageType("error");
-    }
- };
-
- return (
+  return (
     <div>
       <div className="container">
         <SidebarMui />
         <div className="newProduct">
           <h1 className="informacionGeneral-h1">Información General</h1>
-          {message && (
-            <Alert severity={messageType} onClose={() => setMessage("")}>
-              {message}
-            </Alert>
-          )}
-          {submittedData ? (
-            <div>
-              <p>Nombre de la Empresa: {submittedData.nombreEmpresa}</p>
-              <p>Rif de la Empresa: {submittedData.rifEmpresa}</p>
-              <p>Teléfono: {submittedData.telefono}</p>
-              <p>Ubicación: {submittedData.ubicacion}</p>
-              <p>Correo Electrónico: {submittedData.correo}</p>
-              <p>Link de Facebook: {submittedData.linkFacebook}</p>
-              <p>Link de Instagram: {submittedData.linkInstagram}</p>
-            </div>
-          ) : (
-            <form onSubmit={handleSubmit}>
-              <Box sx={{ flexGrow: 1, padding: { xs: 5, sm: 10, md: 10 } }}>
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <TextField
-                      label="Nombre de la Empresa"
-                      name="nombreEmpresa"
-                      fullWidth
-                      margin="normal"
-                      onChange={handleInputChange}
-                    />
-                 </Grid>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <TextField
-                      label="Rif de la Empresa"
-                      name="rifEmpresa"
-                      fullWidth
-                      margin="normal"
-                      onChange={handleInputChange}
-                    />
-                 </Grid>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <TextField
-                      label="Teléfono"
-                      name="telefono"
-                      fullWidth
-                      margin="normal"
-                      onChange={handleInputChange}
-                    />
-                 </Grid>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <TextField
-                      label="Ubicación"
-                      name="ubicacion"
-                      fullWidth
-                      margin="normal"
-                      onChange={handleInputChange}
-                    />
-                 </Grid>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <TextField
-                      label="Correo Electrónico"
-                      name="correo"
-                      fullWidth
-                      margin="normal"
-                      onChange={handleInputChange}
-                    />
-                 </Grid>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <TextField
-                      label="Link de Facebook"
-                      name="linkFacebook"
-                      fullWidth
-                      margin="normal"
-                      onChange={handleInputChange}
-                    />
-                 </Grid>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <TextField
-                      label="Link de Instagram"
-                      name="linkInstagram"
-                      fullWidth
-                      margin="normal"
-                      onChange={handleInputChange}
-                    />
-                 </Grid>
-                 <Grid item xs={12} sm={6} md={6}>
-                    <button className="addProductButton" type="submit">
-                      Guardar
-                    </button>
-                 </Grid>
+
+          <form onSubmit={handleSubmit}>
+            <Box sx={{ flexGrow: 1, padding: { xs: 5, sm: 10, md: 10 } }}>
+              <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid  item xs={12} sm={6} md={6}>
+                  <TextField
+                    label="Nombre de la Empresa"
+                    name="companyName"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputChange}
+                  />
                 </Grid>
-              </Box>
-            </form>
-          )}
+                <Grid  item xs={12} sm={6} md={6}>
+                  <TextField
+                    label="Rif de la empresa"
+                    name="rif"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid  item xs={12} sm={6} md={6}>
+                  <TextField
+                    label="Numero de Telefono"
+                    name="Numero de Telefono"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputChange}
+                  />{" "}
+                </Grid>
+                <Grid  item xs={12} sm={6} md={6}>
+                  <TextField
+                  id="outlined-multiline-static"
+                    label="Ubicacion"
+                    name="Ubicacion"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputChange}
+                    multiline
+                    
+                  />
+                </Grid>
+                <Grid  item xs={12} sm={6} md={6}>
+                  <TextField
+                    label="Correo electronico "
+                    name="Correo electronico"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid  item xs={12} sm={6} md={6}>
+                  <TextField
+                    label="Link de Facebook "
+                    name="Link de Facebook"
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid  item xs={12} sm={6} md={6}>
+                  <TextField
+                    label="Link de Instagram "
+                    name="Link de Instagram "
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid  item xs={12} sm={6} md={6}>
+                  <input
+                    accept="image/*"
+                    style={{ display: "none" }}
+                    id="contained-button-file"
+                    type="file"
+                    onChange={handleFileChange}
+                  />
+                </Grid>
+                <Grid  item xs={12} sm={6} md={6}>
+                  
+                  <button className="addProductButton" type="submit">
+                    Guardar
+                  </button>
+                </Grid>
+                
+              </Grid>
+            </Box>
+          </form>
         </div>
       </div>
     </div>
- );
+  );
 }
--- portal-web/src/components/footer/Footer.jsx
+++ portal-web/src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Footer = () => {
 
-  const [footerGet, setFooterGet] = useState({ nombreEmpresa: "", telefono:"" });
+  const [footerGet, setFooterGet] = useState({ nombreEmpresa: "" });
 
   useEffect(() => {
     
@@ -18,15 +18,8 @@ const Footer = () => {
          const data = response.data.data;
          if (data && data.length > 0) {
            // Assuming you want to display the first company's mission and vision
-           const firstCompany = data[1];
-           setFooterGet({ nombreEmpresa: firstCompany.nombreEmpresa, 
-                          rifEmpresa: firstCompany.rifEmpresa, 
-                          correo: firstCompany.correo,
-                          ubicacion: firstCompany.ubicacion,  
-                          telefono:firstCompany.telefono,
-                          linkFacebook: firstCompany.linkFacebook,
-                          linkInstagram: firstCompany.linkInstagram
-                        });
+           const firstCompany = data[0];
+           setFooterGet({ nombreEmpresa: firstCompany.nombreEmpresa });
          } else {
            console.log("No data found in response");
          }
@@ -56,20 +49,17 @@ const Footer = () => {
         <div className="box">
           <h2>SIGUENOS</h2>
           <div className="red-social">
-            <a href={footerGet.linkFacebook} target="_blank" rel="noopener noreferrer"><FacebookIcon className="sidebarIcon"/></a>
-            <a href={footerGet.linkInstagram} target="_blank" rel="noopener noreferrer"><InstagramIcon className="sidebarIcon"/></a>
+            <a target="_blank" rel="noopener noreferrer"><FacebookIcon className="sidebarIcon"/></a>
+            <a target="_blank" rel="noopener noreferrer"><InstagramIcon className="sidebarIcon"/></a>
           </div>
         </div>
       </div>
       <div className="grupo-2">
-        <small>&copy; 2024 <b>Empresa: {footerGet.nombreEmpresa}  rif:  {footerGet.rifEmpresa}</b></small>
-      </div>
-      <div className="grupo-2">
-        <small><b>Ubicación: {footerGet.ubicacion}   Correo: {footerGet.correo}  </b></small>
+        <small>&copy; 2024 <b>Desarrollado por: {footerGet.nombreEmpresa}</b></small>
       </div>
       <div className="grupo-2">
         <small>
-          Solicitud de Servicio: {footerGet.telefono} 
+          Solicitud de Servicio: 0251-7181107 / Soporte Técnico 0251-7181108
         </small>
       </div>
     </footer>
