30233c94da9557758323f3d080ad0e2466df50a7
hectormg24 <64148157+hectormg24@users.noreply.github.com>
2024-05-15 14:28:52 -0400

    Actualización de perfil de usuario

---
+++ portal-web/src/components/UserProfile/userPerfil.jsx
@@ -0,0 +1,197 @@
+import React, { useState } from 'react';
+import { TextField, Button, IconButton } from '@mui/material';
+import PublishIcon from '@mui/icons-material/Publish';
+import Autocomplete from '@mui/material/Autocomplete';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import CommentIcon from '@mui/icons-material/Comment';
+import './userPerfil.css'
+
+
+const Div = styled('div')(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(1),
+}));
+
+
+const UserPerfil = () => {
+  const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+
+  return (
+    <div className="container-user-profile">
+      <div className="wrapper-user-profile">
+        <div className="user">
+          <div className="userContainer">
+            <div className="userUpdate">
+              <span className="userUpdateTitle">Perfil</span>
+              <form className="userUpdateForm">
+                <div className="userUpdateLeft">
+                  
+                    <div className="userUpdateRight">
+                    <div className="userUpdateUpload">
+                        <img
+                        className="userUpdateImg"
+                        src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                        alt=""
+                        />
+                        <label htmlFor="file">
+                        <IconButton aria-label="upload picture" component="span">
+                            <PublishIcon />
+                        </IconButton>
+                        </label>
+                        <input type="file" id="file" style={{ display: 'none' }} />
+                    </div>
+                    </div>
+                  
+                  
+                  <div className="userUpdateItem">
+                    
+                      <Divider component="div" role="presentation" >
+                        <Typography sx={{
+                          borderRadius:'6px',
+                          border: '1px solid #18a0a6',
+                          margin: '2px',
+                          padding: '3px 10px 3px 10px'
+                        
+                      }}>Información General</Typography>
+                      </Divider>
+
+                      <List component="nav">
+                        <ListItem>
+                          <ListItemText primary="Nombre: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+                        
+
+                        <Divider />
+                        <ListItem>
+                          <ListItemText primary="Apellido: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+
+                        <Divider />
+                        <ListItem>
+                          <ListItemText primary="Cédula: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+
+                        <Divider />
+                        <ListItem>
+                          <ListItemText primary="Fecha de nacimiento: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+                      </List>
+
+
+
+
+                  </div>
+                    
+
+                  <div className="userUpdateItem">
+                    <Divider component="div" role="presentation">
+                      <Typography sx={{
+                          
+                          margin: '2px',
+                          padding: '3px 10px 3px 10px'
+                        
+                      }}>Contacto</Typography>
+                    </Divider>
+
+                    <List component="nav">
+                      <ListItem>
+                        <ListItemText primary="Correo: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+                        <Divider />
+                        <ListItem>
+                          <ListItemText primary="Teléfono: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+                      
+                      <Divider />
+                        <ListItem>
+                          <ListItemText primary="Dirección: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+                      </List>
+
+                  </div>
+
+                  <div className="userUpdateItem1" >
+
+                  
+
+                    <List component="nav">
+
+                      <ListItem sx={{textAlign:'center', 
+                      bgcolor:'#888888', 
+                      marginTop:'-10px', 
+                      borderTopLeftRadius:'12px', 
+                      borderTopRightRadius:'12px',
+                      color:'#fff'}}>
+                        <ListItemText >
+                          Seguridad
+                        </ListItemText>
+                        
+                      </ListItem>
+                        
+
+                      <ListItem>
+                        <ListItemText primary="Usuario: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+                        <Divider />
+                        <ListItem>
+                          <ListItemText primary="Contraseña: " />
+                          <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                          </IconButton>
+                        </ListItem>
+                      </List>
+
+                  </div>
+
+                </div>
+                
+                <Button variant="contained" color="primary" className="userUpdateButton" sx={{alignItems:'center'}}>
+                  Save Changes
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserPerfil;
--- portal-web/src/pages/UserProfile/UserProfile.jsx
+++ portal-web/src/pages/UserProfile/UserProfile.jsx
@@ -1,175 +1,28 @@
 import React from "react";
-import { Container, Typography, Grid, TextField, Button } from "@mui/material";
+
 import UserNavBar from "../../components/UserNavBar/UserNavBar";
 import Footer from "../../components/footer/Footer";
-import { Card, CardContent, Avatar } from "@mui/material";
-import Stack from "@mui/material/Stack";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
+import UserPerfil from "../../components/UserProfile/userPerfil.jsx";
 import './UserProfile.css'
 
 
 
-const Item = styled(Paper)(({}) => ({
-  textAlign: "center",
-}));
+
 
 const UserProfile = () => {
-  const [nombre, setName] = React.useState("Héctor Julio Méndez Gómez");
-  const [cedula, setCedula] = React.useState("262325307");
-  const [email, setEmail] = React.useState("HECTORMG2404@GMAIL.COM");
-  const [FechaNacimiento, setBirthDate] = React.useState("24/04/1998");
-  const [DirUsuario, setAddress] = React.useState(
-    "URB. VISTA ALEGRE, 3ERA ETAPA CASA 001. AV2 2 CON CALLE 17. MUNICIPIO INDEPENDENCIA, ESTADO YARACUY"
-  );
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission
-  };
-
+ 
   return (
     <div className="container-user-profile">
       <UserNavBar />
 
       <div className="wrapper-user-profile">
-        <Stack
-          direction="row"
-          spacing={8}
-          sx={{ marginBottom: "8px", marginTop: "8px", padding:"40px 0 40px 0" }}
-        >
-          <Item>
-            <Container
-              sx={{
-                width: "460px",
-                border: "12px solid #18a0a6",
-                borderRadius: "12px",
-                padding: "40px 0 40px 0",
-                color: "#18a0a6",
-                textAlign: "center",
-              }}
-            >
-              <form onSubmit={handleSubmit}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                  Datos de Usuario
-                </Typography>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <TextField
-                      disabled
-                      id="UserName"
-                      label="Nombre"
-                      value={nombre}
-                      onChange={(e) => setName(e.target.value)}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      disabled
-                      id="cedula"
-                      label="Cédula"
-                      value={cedula}
-                      onChange={(e) => setCedula(e.target.value)}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      disabled
-                      id="email"
-                      label="Email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      disabled
-                      id="UserNacimiento"
-                      label="Fecha de nacimiento"
-                      value={FechaNacimiento}
-                      onChange={(e) => setBirthDate(e.target.value)}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      disabled
-                      id="DirUsuario"
-                      label="Dirección"
-                      value={DirUsuario}
-                      onChange={(e) => setAddress(e.target.value)}
-                      fullWidth
-                      multiline
-                      rows={2}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      fullWidth
-                      sx={{
-                        bgcolor: "#18a0a6",
-                        color: "#fff",
-                        ":hover": { bgcolor: "#fff", color: "#18a0a6" },
-                      }}
-                    >
-                      Modificar Datos
-                    </Button>
-                  </Grid>
-                </Grid>
-              </form>
-            </Container>
-          </Item>
 
-          <Item>
-           
-          </Item>
-        </Stack>
+        <UserPerfil />
 
-        <Stack
-          direction="row"
-          spacing={8}
-          sx={{ marginBottom: "8px", marginTop: "8px", padding:"40px 0 40px 0" }}
-        >   
-
-          <Card
-              sx={{
-                width: "300px",
-                height: "254px",
-                backgroundColor: "teal",
-                marginTop: "80px",
-                paddingTop: "40px",
-              }}
-            >
-                
-              <CardContent>
-                <Avatar
-                  sx={{
-                    bgcolor: "deepOrange[500]",
-                    width: 56,
-                    height: 56,
-                    margin: "0 auto",
-                    marginBottom: "12px",
-                  }}
-                ></Avatar>
-                <Typography
-                  variant="h5"
-                  component="div"
-                  align="center"
-                  color="white"
-                >
-                  Hola, {nombre}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Stack>
-       
       </div>
+      
       <Footer />
+
     </div>
   );
 };
